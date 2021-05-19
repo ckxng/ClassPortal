1bf0eb01aa346fd8aa95aade49eda422001a7996
Cameron King <cking@ckxng.com>
2021-05-19 10:19:21 -0500

    adds record list view

---
+++ ClassPortal/Pages/Records.cshtml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using ClassPortal.Models;
+
+namespace ClassPortal.Pages
+{
+    public class RecordsModel : PageModel
+    {
+        private readonly ClassPortal.Models.DataContext _context;
+
+        public RecordsModel(ClassPortal.Models.DataContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Record> Record { get; set; }
+
+        public async Task OnGetAsync()
+        {
+            Record = await _context.Records
+                .Include("Student")
+                .Include("Semester")
+                .Include("Course")
+                .ToListAsync();
+        }
+    }
+}
