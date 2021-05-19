a35541ea958664bb80136a863fa517b1730e85c1
Cameron King <cking@ckxng.com>
2021-05-19 15:49:58 -0500

    adds record search and lookup

--- ClassPortal/Pages/Records.cshtml.cs
+++ ClassPortal/Pages/Records.cshtml.cs
@@ -13,20 +13,42 @@ namespace ClassPortal.Pages
     {
         private readonly ClassPortal.Models.DataContext _context;
 
+        public string Error = null;
+
         public RecordsModel(ClassPortal.Models.DataContext context)
         {
             _context = context;
         }
 
         public IList<Record> Record { get; set; }
+        public Student Student { get; set; }
 
-        public async Task OnGetAsync()
+        public async Task OnGetAsync(string StudentNumber, string LastName)
         {
+            Student = await _context.Students
+                .Where(s => s.StudentNumber == StudentNumber && s.LastName == LastName)
+                .FirstOrDefaultAsync();
+
+            if(Student == null)
+            {
+                Error = "no student found";
+                return;
+            }
+
             Record = await _context.Records
                 .Include("Student")
+                .Where(r => r.StudentId == Student.Id)
                 .Include("Semester")
                 .Include("Course")
+                .OrderBy(s => s.Semester.Date)
+                .ThenBy(s => s.Course.Code)
                 .ToListAsync();
+
+            if (Record == null)
+            {
+                Error = "no records found";
+                return;
+            }
         }
     }
 }
