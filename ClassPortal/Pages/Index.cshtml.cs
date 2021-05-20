b227135772830adfe76dd7daa437625d65b99482
Cameron King <cking@ckxng.com>
2021-05-19 19:15:31 -0500

    adds college to login

--- ClassPortal/Pages/Index.cshtml.cs
+++ ClassPortal/Pages/Index.cshtml.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ClassPortal.Models;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
 using System;
@@ -12,14 +13,29 @@ namespace ClassPortal.Pages
     {
         private readonly ILogger<IndexModel> _logger;
 
-        public IndexModel(ILogger<IndexModel> logger)
+        private readonly DataContext _context;
+
+        public string Error = null;
+
+        public IList<College> Colleges { get; set; }
+
+        public IndexModel(DataContext context, ILogger<IndexModel> logger)
         {
             _logger = logger;
+            _context = context;
+
+            Colleges = _context.Colleges
+                .OrderBy(c => c.Name)
+                .ToList();
         }
 
         public void OnGet()
         {
-
+            if(Colleges == null)
+            {
+                Error = "unable to retrieve list of colleges";
+                return;
+            }
         }
     }
 }
--- ClassPortal/Pages/Records.cshtml.cs
+++ ClassPortal/Pages/Records.cshtml.cs
@@ -11,11 +11,11 @@ namespace ClassPortal.Pages
 {
     public class RecordsModel : PageModel
     {
-        private readonly ClassPortal.Models.DataContext _context;
+        private readonly DataContext _context;
 
         public string Error = null;
 
-        public RecordsModel(ClassPortal.Models.DataContext context)
+        public RecordsModel(DataContext context)
         {
             _context = context;
         }
@@ -23,7 +23,7 @@ namespace ClassPortal.Pages
         public IList<Record> Record { get; set; }
         public Student Student { get; set; }
 
-        public async Task OnGetAsync(string StudentNumber, string LastName)
+        public async Task OnGetAsync(long CollegeId, string StudentNumber, string LastName)
         {
             Student = await _context.Students
                 .Where(s => s.StudentNumber == StudentNumber && s.LastName == LastName)
@@ -31,14 +31,15 @@ namespace ClassPortal.Pages
 
             if(Student == null)
             {
-                Error = "no student found";
+                Error = "unable to lookup student";
                 return;
             }
 
             Record = await _context.Records
                 .Include("Student")
-                .Where(r => r.StudentId == Student.Id)
                 .Include("Semester")
+                .Where(r => r.StudentId == Student.Id)
+                .Where(r => r.Semester.CollegeId == CollegeId)
                 .Include("Course")
                 .OrderBy(s => s.Semester.Date)
                 .ThenBy(s => s.Course.Code)
@@ -46,7 +47,13 @@ namespace ClassPortal.Pages
 
             if (Record == null)
             {
-                Error = "no records found";
+                Error = "unable to lookup records";
+                return;
+            }
+
+            if(Record.Count == 0)
+            {
+                Error = "this user has no records in this college";
                 return;
             }
         }
